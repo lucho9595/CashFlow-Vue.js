3b73cab73695396b3003b828cb798452453fc769
Luciano Coronel <86379664+lucho9595@users.noreply.github.com>
2023-08-19 12:38:55 -0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <header>
+        <img @click="reload" src="@/assets/cashflow-icon.svg" alt="logo">
+        <h1><span>Cash</span>Flow</h1>
+        <span></span>
+    </header>
+</template>
+
+<script>
+export default {
+    methods: {
+        reload() {
+            location.reload();
+        },
+    },
+}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    width: 100vw;
+}
+
+img,
+span {
+    width: 48px;
+}
+
+h1 {
+    color: var(--brand-blue);
+}
+
+h1 span {
+    color: var(--brand-green);
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <Layout>
+      <template #header>
+        <Header></Header>
+      </template>
+      <template #resume>
+        <Resume :label="label" :total-amount="100000" :amount="amount" :totalLabel="'Ahorro Total'">
+          <template #graphic>
+            graphic
+          </template>
+          <template #action>
+            action
+          </template>
+        </Resume>
+      </template>
+      <template #movements>
+        <Movements />
+      </template>
+    </Layout>
+  </div>
+</template>
+
+<script>
+import Layout from './Layout.vue';
+import Header from './Header.vue';
+import Resume from './Resume/Index.vue';
+import Movements from './Movements/Index.vue';
+
+export default {
+  components: {
+    Layout,
+    Header,
+    Resume,
+    Movements
+  },
+  data() {
+    return {
+      amount: null,
+      label: '18 de Agosto del 2023'
+    }
+  },
+};
+</script>
